e3edcd8e5672e95b0995ed0a26ccf0c270d3d92b
lucafano04 <luca.facchini.it@gmail.com>
2020-12-04 13:17:08 +0100

    Delettle of console.log for debug. V1.0.1 is comming out

--- script.js
+++ script.js
@@ -9,52 +9,52 @@ document.getElementById("nameA").value = obj.Away;
 document.getElementById("colorH").value = obj.ColorH;
 document.getElementById("colorA").value = obj.ColorA;
 function ball(op){
-    if(obj.Ball<3&&op == "+"){
+    if(obj.Ball<3&&op == "+")
         nball = obj.Ball + 1;
-    }else if(obj.Ball>0&&op == "-"){
+    else if(obj.Ball>0&&op == "-")
         nball = obj.Ball - 1;
-    }else if(op == "0"){
+    else if(op == "0")
         nball = 0;
-    }else{
+    else{
         alert("Non puoi eseguire questa operazione");
         return;
     }
     update()
 }
 function strike(op){
-    if(obj.Strike<2&&op == "+"){
+    if(obj.Strike<2&&op == "+")
         nStrike = obj.Strike + 1;
-    }else if(obj.Strike>0&&op == "-"){
+    else if(obj.Strike>0&&op == "-")
         nStrike = obj.Strike - 1;
-    }else if(op == "0"){
+    else if(op == "0")
         nStrike = 0;
-    }else{
+    else{
         alert("Non puoi eseguire questa operazione");
         return;
     }
     update()
 }
 function out(op){
-    if(obj.Out<2&&op == "+"){
+    if(obj.Out<2&&op == "+")
         nOut = obj.Out + 1
-    }else if(obj.Out>0&&op=="-"){
+    else if(obj.Out>0&&op=="-")
         nOut = obj.Out -1;
-    }else if(op == "0"){
+    else if(op == "0")
         nOut = 0;
-    }else{
+    else{
         alert("Non puoi eseguire questa operazione");
         return;
     }
     update()
 }
 function ining(op){
-    if(op == "+"){
+    if(op == "+")
         nIning = obj.Ining + 1;
-    }else if(op == "-"&&obj.Ining > 0){
+    else if(op == "-"&&obj.Ining > 0)
         nIning = obj.Ining - 1;
-    }else if(op == "0"){
+    else if(op == "0")
         nIning = 0;
-    }else{
+    else{
         alert("Non puoi eseguire questa operazione");
         return;
     }
@@ -73,27 +73,24 @@ function nul(){
     update()
 }
 function b1(){
-    if(obj.b1){
+    if(obj.b1)
         obj.b1 = false;
-    }else{
+    else
         obj.b1 = true;
-    }
     update()
 }
 function b2(){
-    if(obj.b2){
+    if(obj.b2)
         obj.b2 = false;
-    }else{
+    else
         obj.b2 = true;
-    }
     update()
 }
 function b3(){
-    if(obj.b3){
+    if(obj.b3)
         obj.b3 = false;
-    }else{
-        obj.b3 = true;
-    }
+    else
+        obj.b3 = true
     update()
 }
 function resetc(){
@@ -118,9 +115,8 @@ function reseti(){
 }
 function reseta(){
     var c=confirm("Una volta eseguita questa operazione non potrai annullarla\nSei Sicuro?");
-    if(c==false){
+    if(c==false)
         return;
-    }
     nball = 0;
     nStrike = 0;
     nOut = 0;
@@ -135,45 +131,34 @@ function reseta(){
     location.reload();
 }
 function sHome(op){
-    if(op == "+"){
+    if(op == "+")
         obj.ScoreH = obj.ScoreH + 1;
-    }
-    if(op == "-"&&obj.ScoreH > 0){
+    else if(op == "-"&&obj.ScoreH > 0)
         obj.ScoreH = obj.ScoreH - 1;
-    }
-    if(op == "0"){
-        obj.ScoreH = 0;
-    }
+    else if(op == "0")
+        obj.ScoreH = 0
     update()
 }
 function sAway(op){
-    if(op == "+"){
+    if(op == "+")
         obj.ScoreA = obj.ScoreA + 1;
-    }
-    if(op == "-"&&obj.ScoreA > 0){
+    else if(op == "-"&&obj.ScoreA > 0)
         obj.ScoreA = obj.ScoreA - 1;
-    }
-    if(op == "0"){
+    else if(op == "0")
         obj.ScoreA = 0;
-    }
     update()
 }
 function update(par){
-    if(nball!=null){
+    if(nball!=null)
         obj.Ball = nball;
-    }
-    if(nStrike!=null){
+    if(nStrike!=null)
         obj.Strike = nStrike;
-    }
-    if(nOut!=null){
+    if(nOut!=null)
         obj.Out = nOut;
-    }
-    if(nIning!=null){
+    if(nIning!=null)
         obj.Ining = nIning
-    }
-    if(nBot!=null){
+    if(nBot!=null)
         obj.bot = nBot
-    }
     obj.Away=document.getElementById("nameA").value;
     obj.Home=document.getElementById("nameH").value;
     obj.ColorA=document.getElementById("colorA").value;
@@ -191,10 +176,7 @@ function update(par){
     xhr.open("POST", './update.php', true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.onreadystatechange = function() { // Call a function when the state changes.
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        var iframe = document.getElementById('scoreboard');
-        iframe.src = iframe.src;
-    }
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {}
     }
     xhr.send("d="+ndata);
 }
