dbb3d15aaa13c8c382003e8d9b2ef151eab2a6d2
Luca Facchini <45771235+lucafano04@users.noreply.github.com>
2020-12-07 18:15:32 +0100

    V1.0.1 (#1)
    * Create pregame.html
    
    * Adding pregame and Updating README
    
    * Pre-game graphic works
    
    Now the pre-game graphic works, you can use this before game start.
    
    * Update README.md
    
    * Post-game graphic works
    
    Now the post-game graphic works, you can use this after the end of the game for sowing the result.
    
    * Update update.php
    
    Aggiunta contollo delle richieste
    
    * Delete gest1.html
    
    * cache bug fix
    
    Now browser cache fixed.
    
    * Update Inning Arrow
    
    Now using 0/1/2 for all variable 0=two dash line 1=top 2=bottom
    
    * Bugfix
    
    Fixed Inning Arrow bug
    
    * LIVE SCOREBOARD
    
    Now the scoreboard updates live.
    
    * Update README.md
    
    Update README.md for the new version.
    
    * Update postgame.html
    
    * Delete of console.log for debugging.
    
    * Update of README.md added Installation and Italian translation

--- script.js
+++ script.js
@@ -9,91 +9,88 @@ document.getElementById("nameA").value = obj.Away;
 document.getElementById("colorH").value = obj.ColorH;
 document.getElementById("colorA").value = obj.ColorA;
 function ball(op){
-    if(obj.Ball<3&&op == "+"){
+    if(obj.Ball<3&&op == "+")
         nball = obj.Ball + 1;
-    }else if(obj.Ball>0&&op == "-"){
+    else if(obj.Ball>0&&op == "-")
         nball = obj.Ball - 1;
-    }else if(op == "0"){
+    else if(op == "0")
         nball = 0;
-    }else{
+    else{
         alert("Non puoi eseguire questa operazione");
         return;
     }
     update()
 }
-function nul(){
-    obj.bot = null;
-    update()
-}
 function strike(op){
-    if(obj.Strike<2&&op == "+"){
+    if(obj.Strike<2&&op == "+")
         nStrike = obj.Strike + 1;
-    }else if(obj.Strike>0&&op == "-"){
+    else if(obj.Strike>0&&op == "-")
         nStrike = obj.Strike - 1;
-    }else if(op == "0"){
+    else if(op == "0")
         nStrike = 0;
-    }else{
+    else{
         alert("Non puoi eseguire questa operazione");
         return;
     }
     update()
 }
 function out(op){
-    if(obj.Out<2&&op == "+"){
+    if(obj.Out<2&&op == "+")
         nOut = obj.Out + 1
-    }else if(obj.Out>0&&op=="-"){
+    else if(obj.Out>0&&op=="-")
         nOut = obj.Out -1;
-    }else if(op == "0"){
+    else if(op == "0")
         nOut = 0;
-    }else{
+    else{
         alert("Non puoi eseguire questa operazione");
         return;
     }
     update()
 }
 function ining(op){
-    if(op == "+"){
+    if(op == "+")
         nIning = obj.Ining + 1;
-    }else if(op == "-"&&obj.Ining > 0){
+    else if(op == "-"&&obj.Ining > 0)
         nIning = obj.Ining - 1;
-    }else if(op == "0"){
+    else if(op == "0")
         nIning = 0;
-    }else{
+    else{
         alert("Non puoi eseguire questa operazione");
         return;
     }
     update()
 }
 function top1(){
-    nBot = false
+    obj.bot = 2
     update()
 }
 function bot(){
-    nBot = true
+    obj.bot = 1
+    update()
+}
+function nul(){
+    obj.bot = 0;
     update()
 }
 function b1(){
-    if(obj.b1){
+    if(obj.b1)
         obj.b1 = false;
-    }else{
+    else
         obj.b1 = true;
-    }
     update()
 }
 function b2(){
-    if(obj.b2){
+    if(obj.b2)
         obj.b2 = false;
-    }else{
+    else
         obj.b2 = true;
-    }
     update()
 }
 function b3(){
-    if(obj.b3){
+    if(obj.b3)
         obj.b3 = false;
-    }else{
-        obj.b3 = true;
-    }
+    else
+        obj.b3 = true
     update()
 }
 function resetc(){
@@ -108,19 +105,18 @@ function reseti(){
     obj.b1 = false;
     obj.b2 = false;
     obj.b3 = false;
-    if(obj.bot){
+    if(obj.bot==1){
         nIning = obj.Ining + 1;
-        nBot = false;
+        nBot = 2;
     }else{
-        nBot = true;
+        nBot = 1;
     }
     update()
 }
 function reseta(){
     var c=confirm("Una volta eseguita questa operazione non potrai annullarla\nSei Sicuro?");
-    if(c==false){
+    if(c==false)
         return;
-    }
     nball = 0;
     nStrike = 0;
     nOut = 0;
@@ -135,45 +131,34 @@ function reseta(){
     location.reload();
 }
 function sHome(op){
-    if(op == "+"){
+    if(op == "+")
         obj.ScoreH = obj.ScoreH + 1;
-    }
-    if(op == "-"&&obj.ScoreH > 0){
+    else if(op == "-"&&obj.ScoreH > 0)
         obj.ScoreH = obj.ScoreH - 1;
-    }
-    if(op == "0"){
-        obj.ScoreH = 0;
-    }
+    else if(op == "0")
+        obj.ScoreH = 0
     update()
 }
 function sAway(op){
-    if(op == "+"){
+    if(op == "+")
         obj.ScoreA = obj.ScoreA + 1;
-    }
-    if(op == "-"&&obj.ScoreA > 0){
+    else if(op == "-"&&obj.ScoreA > 0)
         obj.ScoreA = obj.ScoreA - 1;
-    }
-    if(op == "0"){
+    else if(op == "0")
         obj.ScoreA = 0;
-    }
     update()
 }
 function update(par){
-    if(nball!=null){
+    if(nball!=null)
         obj.Ball = nball;
-    }
-    if(nStrike!=null){
+    if(nStrike!=null)
         obj.Strike = nStrike;
-    }
-    if(nOut!=null){
+    if(nOut!=null)
         obj.Out = nOut;
-    }
-    if(nIning!=null){
+    if(nIning!=null)
         obj.Ining = nIning
-    }
-    if(nBot!=null){
+    if(nBot!=null)
         obj.bot = nBot
-    }
     obj.Away=document.getElementById("nameA").value;
     obj.Home=document.getElementById("nameH").value;
     obj.ColorA=document.getElementById("colorA").value;
@@ -191,10 +176,7 @@ function update(par){
     xhr.open("POST", './update.php', true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.onreadystatechange = function() { // Call a function when the state changes.
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        var iframe = document.getElementById('scoreboard');
-        iframe.src = iframe.src;
-    }
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {}
     }
     xhr.send("d="+ndata);
 }
--- update.php
+++ update.php
@@ -1,8 +1,9 @@
 <?php
-$myfile = fopen("data.json", "w") or die("Unable to open file!");
-$txt = $_POST[d];
-fwrite($myfile, $txt);
-fclose($myfile);
-echo $_GET[d];
-echo $_POST[d];
+if(isset($_POST[d])){
+    $myfile = fopen("data.json", "w") or die("Unable to open file!");
+    $txt = $_POST[d];
+    fwrite($myfile, $txt);
+    fclose($myfile);
+    echo $_POST[d];
+}
 ?> 
