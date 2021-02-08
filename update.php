239efcf561ee15375fd20f12d2392f165a00d3f2
Luca Facchini <45771235+lucafano04@users.noreply.github.com>
2021-02-08 10:40:26 +0100

    Creating API response
    Creating responses for Query to update.php

--- update.php
+++ update.php
@@ -1,9 +1,18 @@
 <?php
-if(isset($_POST[d])){
-    $myfile = fopen("data.json", "w") or die("Unable to open file!");
-    $txt = $_POST[d];
-    fwrite($myfile, $txt);
-    fclose($myfile);
-    echo $_POST[d];
+if(isset($_POST['d'])){
+    $file = fopen("data.json", "w");
+    if($file==false){
+        echo('{"ok":false,"code":500,"result":"Unable to open file"}');
+        http_response_code(500);
+    }else{
+        $txt = $_POST['d'];
+        fwrite($file, $txt);
+        fclose($file);
+        echo('{"ok":true,"code":200,"result":"changes done"}');
+        http_response_code(200);
+    }
+}else{
+    echo('{"ok":false,"code":400,"result":"d for data set is not set"}');
+    http_response_code(400);
 }
 ?> 
