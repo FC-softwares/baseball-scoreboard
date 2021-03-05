dde6c7840a3fa76e90834bcd7d1f83cfebf81841
Luca Facchini <45771235+lucafano04@users.noreply.github.com>
2021-03-05 18:03:20 +0100

    V1.1.0 (#5)
    * Added index
    
    Added index.html for redirecting to all pages of overlay, next we will include a small guide
    
    * using fetch()
    
    now using function fetch() for getting JSON admin and other files will be updated later
    
    * Completed fetch()
    
    Completed implementation of function fetch() for getting data and for saving network usange
    
    * Update README.md
    
    * add LICENSE
    
    separeted README and LICENSE
    
    * Fixed index.html
    
    Fiked link bug on index.html
    
    * updated README.md
    
    * removed debug
    
    Removed all console.log() function used for debugging
    
    * Removed dashes
    
    Removed dashes because no longer necessary use overlays for pregame and postgame
    
    * Creating API response
    
    Creating responses for Query to update.php
    
    * API response
    
    Improved api response and http_response_code for code 400,500,405 for update.php
    
    * Added log for changes
    
    Added log for change work in progress
    
    * Update .gitignore
    
    * Added data control
    
    The control of the new data passed to update.php has been added with its response 400 with a new parameter 'missing' where the missing parameters are listed.
    
    * Update format.txt
    
    Updated the file format.txt with the new file format.
    
    * Update script.js
    
    Fixed a bug on functions top1() and bot() now update and the arrow works correctly.
    
    * Fixed bug in response
    
    Fixed bug in response, now all responses will be handled as application/json so the request is recognized as json.
    Also fixed "200" responses now show changes in the "changes" parameter
    
    * Emptied the log
    
    * Style for V1.1.0 (#4)
    
    * Fixed index and added footer
    
    - Fixed grammar errors in index.html
    - Added a footer in index.html and style_index.css
    
    * Updated index HTML and CSS
    
    Redesigned the index.html page.
    
    * Updated layout for index and fixed translations
    
    Added separators for index.html (needed for a future revision) and translated Italian texts to English. Fixed issue with footer going outside of the viewport. Added footer to admin.html.
    
    * Separeted changelog and Readme
    
    We have separeted the changelog out of readme for better reading into the index
    
    * Footer changed
    
    Now the footer will open a pop-up box for the visualization of README.md and LICENSE, now both files can be viewed directly within the browser.
    
    * Added navbar to header
    
    Added a horizontal navbar to the header, made a vertical navbar when the screen is smaller than 600px and fixed the footer hiding content.
    
    * Fixed TOP/BOT cell
    
    Fixed cell TOP/BOT having a higher colspan than normal and fixed the padding of the BOT button.
    
    * Started Copy
    
    Started developing of copy function for overlay links
    
    * Fixed Modal
    
    Fixed a bug for mobile devices, now you can close the popup of README.md
    
    * Added copy function
    
    Added copy function for overlay links via a button to the right of the overlay links.
    
    Co-authored-by: TheTecnoKing <kristian.16122005@gmail.com>
    
    * Update changelog.md
    
    Changelog updated for the publication of the new version (V1.1.0)
    
    * Update README.md
    
    * Update format.txt
    
    * Fixed grammar mistakes in changelog and README.
    
    * Fixed translation and grammar mistakes in changelog
    
    Co-authored-by: TheTecnoKing <kristian.16122005@gmail.com>

--- update.php
+++ update.php
@@ -1,9 +1,69 @@
 <?php
-if(isset($_POST[d])){
-    $myfile = fopen("data.json", "w") or die("Unable to open file!");
-    $txt = $_POST[d];
-    fwrite($myfile, $txt);
-    fclose($myfile);
-    echo $_POST[d];
+header('Content-Type: application/json');
+if($_SERVER['REQUEST_METHOD']==='POST'){
+    if(isset($_POST['d'])){
+        $old1=file_get_contents("./data.json");
+        $old=json_decode($old1, true);
+        //Saving array types required
+        $types=array();
+        foreach ($old as $key => $value) {
+            $types[]=$key;
+        }
+        $file = fopen("data.json", "w");
+        if($file==false){
+            echo('{"ok":false,"code":500,"result":"Unable to open file"}');
+            http_response_code(500);
+        }else{
+            $new=json_decode($_POST['d'],true);
+            //Checking passed items
+            $i=0;
+            $missing=array();
+            $types_new=array();
+            foreach ($new as $key => $value) {
+                $types_new[]=$key;
+            }
+            foreach ($types as $key => $value) {
+                if(in_array($value,$types_new)){
+                    $i++;
+                }else{
+                    $missing[]=$value;
+                }
+            }
+            //I check if they are all present
+            if($i==14){
+                //comparing difference
+                $diff=array_diff_assoc($new,$old);
+                $diff=json_encode($diff,true);
+                //Saving JSON
+                fwrite($file, $_POST['d']);
+                fclose($file);
+                //oppening LOG
+                $log=fopen("changes.log",'a');
+                if($log==false){
+                    echo('{"ok":true,"code":200,"result":"changes done","changes":'.$diff.',"log":false}');
+                    http_response_code(200);
+                }else{
+                    //Saving log
+                    $txt_log="[INFO] ".date("d-m-Y H:i:s")." chages done: ".$diff."\n";
+                    fwrite($log,$txt_log);
+                    fclose($log);
+                    echo('{"ok":true,"code":200,"result":"changes done","changes":'.$diff.',"log":true}');
+                    http_response_code(200);
+                }
+            }else{
+                $missing_txt=json_encode($missing,true);
+                echo('{"ok":false,"code":400,"result":"d is not correctly set","missiing":"'.$missing_txt.'"}');
+                http_response_code(400);
+                fwrite($file,$old1);
+                fclose($file);
+            }
+        }
+    }else{
+        echo('{"ok":false,"code":400,"result":"d for data set is not set"}');
+        http_response_code(400);
+    }
+}else{
+    echo('{"ok":false,"code":405,"result":"'.$_SERVER['REQUEST_METHOD'].' request not allowed"}');
+    http_response_code(405);
 }
 ?> 
