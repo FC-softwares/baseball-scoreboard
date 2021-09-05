e3f1b1fc1a697cb100368f04d5f255b89e5132db
Luca Facchini <45771235+lucafano04@users.noreply.github.com>
2021-09-05 09:00:20 +0200

    Added Front-End code for generating token and send to server

--- auth.js
+++ auth.js
@@ -40,13 +40,59 @@ function submit(new_p,old,opt){
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.onreadystatechange = function() { // Call a function when the state changes.
         if (this.readyState === XMLHttpRequest.DONE && this.status != 200) {
-            window.alert("An error occurred while updating score\nSee console for more information\nERROR code: "+this.status);
+            window.alert("An error occurred while updating password\nSee console for more information\nERROR code: "+this.status);
             console.debug("Error while sending request\nCODE:"+this.status+"\nRECIVED: '"+this.response+"'");
             
         }else{
-            if(opt="sub")
+            if(opt="sub"&&this.status==200&&this.readyState === XMLHttpRequest.DONE)
                 window.location.replace("./auth.html");
         }
     }
     xhr.send(ndata);
+}
+function update2(){
+    const digest = async ({ algorithm = "SHA-256", message }) =>
+        Array.prototype.map
+          .call(
+            new Uint8Array(
+              await crypto.subtle.digest(algorithm, new TextEncoder().encode(message))
+            ),
+            (x) => ("0" + x.toString(16)).slice(-2)
+          )
+          .join("");
+    
+    old=document.getElementById("passwd").value;
+    new_t=Math.random().toString(16).substr(2, 8);;
+
+    if(document.getElementById("opt1_2").checked)
+        opt="sub";
+    else if(document.getElementById("opt2_2").checked)
+        opt="add";
+    else
+        return;
+    digest({message: new_t})
+          .then(ins=>{
+                new_t=ins;
+                if(submit2(new_t,old,opt))
+                    document.getElementById("token_psh").value=new_t;
+          })
+}
+
+function submit2(new_p,old,opt){
+    ndata="old="+old+"&new="+new_p+"&ope="+opt;
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", './update_token.php', true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() { // Call a function when the state changes.
+        if (this.readyState === XMLHttpRequest.DONE && this.status != 200) {
+            window.alert("An error occurred while updating score\nSee console for more information\nERROR code: "+this.status);
+            console.debug("Error while sending request\nCODE:"+this.status+"\nRECIVED: '"+this.response+"'");
+            
+        }else{
+            if(opt="sub"&&this.status==200&&this.readyState === XMLHttpRequest.DONE)
+                window.location.replace("./auth.html");
+        }
+    }
+    xhr.send(ndata);
+    return true;
 }
