556a8bccdee767b1152d27a7cb825ec838458b03
Luca Facchini <45771235+lucafano04@users.noreply.github.com>
2021-11-23 15:29:20 +0100

    Started updating json
    started the integration with the json file with a switch case
    P.S. to be valuteted if is better to switch first on the index and than on the operator

--- app/js/admin.js
+++ app/js/admin.js
@@ -2,38 +2,44 @@ const token = localStorage.getItem('token');
 const user = localStorage.getItem('user');
 
 if (token && user) {
-  const xmlt = new XMLHttpRequest();
-  xmlt.open('POST', 'https://api.facchini-pu.it/checkstat', true);
-  xmlt.setRequestHeader('Content-Type', 'application/json');
-  xmlt.send(`{"id":"${user}","token":"${token}"}`);
-  xmlt.onload = function() {
-	if (xmlt.status === 200) {
-	  const response = JSON.parse(xmlt.responseText);
-	  if (response.ok === true) {
-		const socket = io({
-		  	auth: {
-				id: user,
-				token: token
+	const xmlt = new XMLHttpRequest();
+	xmlt.open('POST', 'https://api.facchini-pu.it/checkstat', true);
+	xmlt.setRequestHeader('Content-Type', 'application/json');
+	xmlt.send(`{"id":"${user}","token":"${token}"}`);
+	xmlt.onload = function() {
+		if (xmlt.status === 200) {
+			const response = JSON.parse(xmlt.responseText);
+			if (response.ok === false) {
+				window.location.href = '/login.html';
+				localStorage.removeItem('token');
+				localStorage.removeItem('user');
 			}
-		});
-		socket.emit('update_data', {
-			test:true
-		});
-	  } else {
-		// User is not in a valid session
-		// Redirect to login page
-		localStorage.removeItem('token');
-		localStorage.removeItem('user');
-	  }
-	} else {
-	  // User is not in a valid session
-	  // Redirect to login page
-	  if (xmlt.status === 500) {
-		alert('Errore: ' + xmlt.status+"\n"+JSON.parse(xmlt.responseText));
-	  }
-	  localStorage.removeItem('token');
-	  localStorage.removeItem('user');
+		} else {
+			// User is not in a valid session
+			// Redirect to login page
+			if (xmlt.status === 500) {
+				alert('Errore: ' + xmlt.status+"\n"+JSON.parse(xmlt.responseText));
+			}
+			window.location.href = '/login.html';
+			localStorage.removeItem('token');
+			localStorage.removeItem('user');
+		}
 	}
-  }
+}else{
+	//redirect to login page
+	window.location.href = '/login.html';
 }
-// Check if the user is in a valid session via API
+
+const socket = io({
+	auth: {
+	  id: user,
+	  token: token
+  	}
+});
+//To add for all the variables
+function Ball(opr){
+	if(opr!="+"&&opr!="-"&&opr!="0")
+		return NaN;
+	socket.emit('update_data', `{"Ball":"${opr}"}`);
+	return true;
+}
--- index.js
+++ index.js
@@ -29,7 +29,6 @@ io.on('connection', (socket) => {
 	console.log('a user connected');
 	//console.log(socket.handshake.auth);
 	socket.on('update_data', (data) => {			
-		console.log("update data recived:"+JSON.stringify(data));
 		if (socket.handshake.auth.id && socket.handshake.auth.token) {
 			const ver_options = {
 				hostname: 'api.facchini-pu.it',
@@ -50,7 +49,57 @@ io.on('connection', (socket) => {
 					//process.stdout.write(d);
 					res_data = JSON.parse(d);
 					if (res_data.ok === true) {
-						console.log('Update_data Autenticated');
+						fs.readFile(__dirname + '/app/json/data.json', 'utf8', (err, data_old) => {
+							if (err) throw err;
+							var json = JSON.parse(data);
+							var data_old_obj = JSON.parse(data_old);
+							Object.entries(json).forEach(entry => {
+								const [indx, element] = entry;
+								switch(element) { 														// switch case to be inpreved for each variable PS meaby is better to switch with index and than operator
+									case '+':
+										switch(indx) {
+											case 'Ball':
+												if (data_old_obj.Ball<3)
+													data_old_obj[indx]++;
+												break;
+											case 'Strike':
+												if (data_old_obj[indx]<2)
+													data_old_obj[indx]++;
+												break;
+											case 'Out':
+												if (data_old_obj[indx]<2)
+													data_old_obj[indx]++;
+												break;
+											default:
+												data_old_obj[indx]++;
+												break;
+										}
+										break;
+									case '-':
+										if(data_old_obj[indx]>0)
+											data_old_obj[indx]--;
+										break;
+									case '0':
+										data_old_obj[indx]=0;
+										break;
+									case false:
+										data_old_obj[indx]=false;
+										break;
+									case true:
+										data_old_obj[indx]=true;
+										break;
+									default:
+										data_old_obj[indx]=element;
+										break;
+									}
+							});	
+							fs.writeFile(__dirname + '/app/json/data.json', JSON.stringify(data_old_obj, null, 4), (err) => {
+								if (err) throw err;
+							});
+							socket.emit('update', data);
+						});
+					}else{
+						
 					}
 				});
 			});
