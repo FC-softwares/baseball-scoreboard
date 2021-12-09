d3be5f5b08c67c08d40aecafe155f9f884eff5a8
Luca Facchini <45771235+lucafano04@users.noreply.github.com>
2021-12-09 17:34:01 +0100

    Added switch on login
    Added switch between not Login state with the login button displayed and the login state with the name of the user and a short menu with logout and profile settings

--- app/js/all.js
+++ app/js/all.js
@@ -3,6 +3,7 @@
 function LogOut(){
 	localStorage.removeItem("user");
 	localStorage.removeItem("token");
+	location.reload();
 }
 
 function CeackSession(){
@@ -20,6 +21,8 @@ function CeackSession(){
 					//modify the HTML based on the user
 					const { name, surname, email} = response.user
 					//document.getElementById('user_name').innerHTML = name;
+					//var profileHTML = ``;
+					document.getElementById('profile_plc').innerHTML = `<span class="dropdown"><a class="dropdown-toggle text-dark text-decoration-none" href="#" id="navScoreboardLinks" role="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">Hi <strong>${name} ${surname}</strong> </a><ul class="dropdown-menu mb-2" aria-labelledby="navScoreboardLinks"><li><a class="dropdown-item" href="#">Profile <i class="bi-box-arrow-up-right"></i></a></li><li><a class="dropdown-item" href="#" title="logout" onclick="LogOut();return false;">logout</a></li></ul></span>`;
 				}
 			}else{
 				if (this.readyState === 4) {
@@ -27,5 +30,8 @@ function CeackSession(){
 				}
 			}
 		}
+	}else{
+		return false;
 	}
 }
+CeackSession();
