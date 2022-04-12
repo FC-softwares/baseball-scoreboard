493450fdac552b156c8eede6a598ded3d259c73f
Luca Facchini <45771235+lucafano04@users.noreply.github.com>
2022-04-12 16:45:23 +0200

    refactor: :recycle: Refactoreted the main switch case statement
    Refactoreted the main switch case statement and converted in some nested if else statement

--- index.js
+++ index.js
@@ -51,174 +51,118 @@ io.on('connection', (socket) => {
 							var data_old_obj = JSON.parse(data_old);
 							Object.entries(json).forEach(entry => {
 								const [indx, element] = entry;
-								switch(element) { 														// switch case to be inpreved for each variable PS meaby is better to switch with index and than operator
-									case '+':
-										switch(indx) {
-											case 'Ball':
-												if (data_old_obj.Ball<3)
-													data_old_obj[indx]++;
-												break;
-											case 'Strike':
-												if (data_old_obj[indx]<2)
-													data_old_obj[indx]++;
-												break;
-											case 'Out':
-												if (data_old_obj[indx]<2)
-													data_old_obj[indx]++;
-												break;
-											case 'Teams.Away.Score':
-													data_old_obj.Int[data_old_obj.Inning].A++;
-													var ScoreATmp = 0;
-													for(var i=1;i<=data_old_obj.Inning;i++) {
-														ScoreATmp += data_old_obj.Int[i].A;
-													}
-													data_old_obj.Teams.Away.Score = ScoreATmp;
-													break;
-											case 'Teams.Home.Score':
-													data_old_obj.Int[data_old_obj.Inning].H++;
-													var ScoreHTmp = 0;
-													for(var i=1;i<=data_old_obj.Inning;i++) {
-														ScoreHTmp += data_old_obj.Int[i].H;
-													}
-													data_old_obj.Teams.Home.Score = ScoreHTmp;
-													break;
-											case 'Inning':
-												data_old_obj.Inning++;
-												data_old_obj.Int[data_old_obj.Inning] = {A: 0,H: 0};
-												break;
-											default:
-												data_old_obj[indx]++;
-												break;
+								if(element === '+'){
+									var { ScoreATmp, ScoreHTmp, i } = plusChanges();
+								}else if(element === '-'){
+									var { ScoreATmp, ScoreHTmp, i } = minusChanges();
+								}else if(element === '0'){
+									var i = zeroChanges();
+								}else if(element === 'toggle'){
+									toggleChanges();
+								}else if(indx === 'Teams.Away.Name')
+									data_old_obj.Teams.Away.Name = element;
+								else if(indx === 'Teams.Home.Name')
+									data_old_obj.Teams.Home.Name = element;
+								else if(indx === 'Teams.Away.Color')
+									data_old_obj.Teams.Away.Color = element;
+								else if(indx === 'Teams.Home.Color')
+									data_old_obj.Teams.Home.Color = element;
+								else
+									data_old_obj[indx] = element;
+
+								function toggleChanges() {
+									if (indx === '1')
+										data_old_obj.Bases[1] = !data_old_obj.Bases[1];
+									else if (indx === '2')
+										data_old_obj.Bases[2] = !data_old_obj.Bases[2];
+									else if (indx === '3')
+										data_old_obj.Bases[3] = !data_old_obj.Bases[3];
+									else if (indx === 'Auto_Change_Inning') {
+										data_old_obj.Bases[1] = false;
+										data_old_obj.Bases[2] = false;
+										data_old_obj.Bases[3] = false;
+										data_old_obj.Ball = 0;
+										data_old_obj.Strike = 0;
+										data_old_obj.Out = 0;
+										if (data_old_obj.Arrow == 1) {
+											data_old_obj.Arrow = 2;
+										} else {
+											data_old_obj.Arrow = 1;
+											data_old_obj.Inning++;
+											data_old_obj.Int[data_old_obj.Inning] = { A: 0, H: 0 };
 										}
-										break;
-									case '-':
-										switch(indx){
-											case 'Teams.Home.Score':
-												if(data_old_obj.Int[data_old_obj.Inning].H>0){
-													data_old_obj.Int[data_old_obj.Inning].H--;
-													data_old_obj.Teams.Home.Score--;
-												}
-												break;
-											case 'Teams.Away.Score':
-												if(data_old_obj.Int[data_old_obj.Inning].A>0){
-													data_old_obj.Int[data_old_obj.Inning].A--;
-													data_old_obj.Teams.Away.Score--;
-												}
-												break;
-											case 'Inning':
-												if(data_old_obj.Inning>1){
-													delete data_old_obj.Int[data_old_obj.Inning];
-													data_old_obj.Inning--;
-													var ScoreATmp = 0, ScoreHTmp = 0;
-													for(var i=1;i<=data_old_obj.Inning;i++){
-														ScoreATmp += data_old_obj.Int[i].A;
-														ScoreHTmp += data_old_obj.Int[i].H;
-													}
-													data_old_obj.Teams.Away.Score = ScoreATmp;
-													data_old_obj.Teams.Home.Score = ScoreHTmp;
-												}
-												break;
-											default:
-												if(data_old_obj[indx]>0)
-													data_old_obj[indx]--;
-												break;
-										}										
-										break;
-									case '0':
-										switch(indx) {	
-											case 'Ball':
-												data_old_obj[indx] = 0;
-												break;
-											case 'Strike':
-												data_old_obj[indx] = 0;
-												break;
-											case 'Out':
-												data_old_obj[indx] = 0;
-												break;
-											case 'Inning':
-												data_old_obj[indx] = 1;
-												data_old_obj.Int={1:{A:0,H:0}};
-												data_old_obj.Teams.Away.Score = 0;
-												data_old_obj.Teams.Home.Score = 0;
-												break;
-											case 'Teams.Away.Score':
-												data_old_obj.Teams.Away.Score = 0;
-												for (var i = 1; i <= data_old_obj.Inning; i++) {
-													data_old_obj.Int[i].A = 0;
-												}
-												break;
-											case 'Teams.Home.Score':
-												data_old_obj.Teams.Home.Score = 0;
-												for (var i = 1; i <= data_old_obj.Inning; i++) {
-													data_old_obj.Int[i].H = 0;
-												}
-												break;
-											default:
-												break;
-											
-										}
-										break;
-									case "toggle":
-										switch(indx){
-											case '1':
-												if(data_old_obj.Bases[1]==false)
-													data_old_obj.Bases[1]=true;
-												else
-													data_old_obj.Bases[1]=false;
-											break;
-											case '2':
-												if(data_old_obj.Bases[2]==false)
-													data_old_obj.Bases[2]=true;
-												else
-													data_old_obj.Bases[2]=false;
-											break;
-											case '3':
-												if(data_old_obj.Bases[3]==false)
-													data_old_obj.Bases[3]=true;
-												else
-													data_old_obj.Bases[3]=false;
-											break;
-											case 'Auto_Change_Inning':
-												data_old_obj.Bases[1]=false;
-												data_old_obj.Bases[2]=false;
-												data_old_obj.Bases[3]=false;
-												data_old_obj.Ball=0;
-												data_old_obj.Strike=0;
-												data_old_obj.Out=0;
-												if(data_old_obj.Arrow==1){
-													data_old_obj.Arrow=2;
-												}else{
-													data_old_obj.Arrow=1;
-													data_old_obj.Inning++;
-													data_old_obj.Int[data_old_obj.Inning]={A:0,H:0};
-												}
-											break;
-											case 'Reset_All':
-												data_old_obj={"Teams": {"Away": {"Name": "AWAY","Score": 0,"Color": "#000000"},"Home": {"Name": "HOME","Score": 0,"Color": "#000000"}},"Ball": 0,"Strike": 0,"Out": 0,"Inning": 1,"Arrow": 1,"Bases": {"1": false,"2": false,"3": false},"Int": {"1": {"A": 0,"H": 0}}};
-												
-											break;
-										}
-										break;
-										default:
-											switch(indx){
-												case 'Teams.Away.Name':
-													data_old_obj.Teams.Away.Name=element;
-													break;
-												case 'Teams.Home.Name':
-													data_old_obj.Teams.Home.Name=element;
-													break;
-												case 'Teams.Away.Color':
-													data_old_obj.Teams.Away.Color=element;
-													break;
-												case 'Teams.Home.Color':
-													data_old_obj.Teams.Home.Color=element;
-													break;
-												default:
-													data_old_obj[indx]=element;
-													break;
-											}
-											break;
+									} else if (indx === 'Reset_All')
+										data_old_obj = { "Teams": { "Away": { "Name": "AWAY", "Score": 0, "Color": "#000000" }, "Home": { "Name": "HOME", "Score": 0, "Color": "#000000" } }, "Ball": 0, "Strike": 0, "Out": 0, "Inning": 1, "Arrow": 1, "Bases": { "1": false, "2": false, "3": false }, "Int": { "1": { "A": 0, "H": 0 } } };
+								}
+								function zeroChanges() {
+									if (indx === 'Inning') {
+										data_old_obj[indx] = 1;
+										data_old_obj.Int = { 1: { A: 0, H: 0 } };
+										data_old_obj.Teams.Away.Score = 0;
+										data_old_obj.Teams.Home.Score = 0;
+									} else if (indx === 'Teams.Away.Score') {
+										data_old_obj.Teams.Away.Score = 0;
+										for (var i = 1; i <= data_old_obj.Inning; i++)
+											data_old_obj.Int[i].A = 0;
+									} else if (indx === 'Teams.Home.Score') {
+										data_old_obj.Teams.Home.Score = 0;
+										for (var i = 1; i <= data_old_obj.Inning; i++)
+											data_old_obj.Int[i].H = 0;
 									}
+									else
+										data_old_obj[indx] = 0;
+									return i;
+								}
+								function minusChanges() {
+									if (indx === 'Teams.Home.Score' && data_old_obj.Int[data_old_obj.Inning].H > 0) {
+										data_old_obj.Int[data_old_obj.Inning].H--;
+										data_old_obj.Teams.Home.Score--;
+									} else if (indx === 'Teams.Away.Score' && data_old_obj.Int[data_old_obj.Inning].A > 0) {
+										data_old_obj.Int[data_old_obj.Inning].A--;
+										data_old_obj.Teams.Away.Score--;
+									} else if (indx === 'Inning') {
+										if (data_old_obj.Inning > 1) {
+											delete data_old_obj.Int[data_old_obj.Inning];
+											data_old_obj.Inning--;
+											var ScoreATmp = 0, ScoreHTmp = 0;
+											for (var i = 1; i <= data_old_obj.Inning; i++) {
+												ScoreATmp += data_old_obj.Int[i].A;
+												ScoreHTmp += data_old_obj.Int[i].H;
+											}
+											data_old_obj.Teams.Away.Score = ScoreATmp;
+											data_old_obj.Teams.Home.Score = ScoreHTmp;
+										}
+									} else if (data_old_obj[indx] > 0)
+										data_old_obj[indx]--;
+									return { ScoreATmp, ScoreHTmp, i };
+								}
+								function plusChanges() {
+									if (indx === 'Ball' && data_old_obj.Ball < 3)
+										data_old_obj[indx] = data_old_obj[indx] + 1;
+									else if (indx === 'Strike' && data_old_obj.Strike < 2)
+										data_old_obj[indx]++;
+									else if (indx === 'Out' && data_old_obj.Out < 2)
+										data_old_obj[indx]++;
+									else if (indx === 'Teams.Away.Score') {
+										data_old_obj.Int[data_old_obj.Inning].A++;
+										var ScoreATmp = 0;
+										for (var i = 1; i <= data_old_obj.Inning; i++) {
+											ScoreATmp += data_old_obj.Int[i].A;
+										}
+										data_old_obj.Teams.Away.Score = ScoreATmp;
+									} else if (indx === 'Teams.Home.Score') {
+										data_old_obj.Int[data_old_obj.Inning].H++;
+										var ScoreHTmp = 0;
+										for (var i = 1; i <= data_old_obj.Inning; i++) {
+											ScoreHTmp += data_old_obj.Int[i].H;
+										}
+										data_old_obj.Teams.Home.Score = ScoreHTmp;
+									} else if (indx === 'Inning') {
+										data_old_obj.Inning++;
+										data_old_obj.Int[data_old_obj.Inning] = { A: 0, H: 0 };
+									}
+									return { ScoreATmp, ScoreHTmp, i };
+								}
 							});
 							fs.writeFile(__dirname + '/app/json/data.json', JSON.stringify(data_old_obj, null, 4), (err) => {
 								if (err) throw err;
