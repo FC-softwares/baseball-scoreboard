fef499f60b3986b324bd37a1c6bb8e9dcacf2c44
Luca Facchini <45771235+lucafano04@users.noreply.github.com>
2023-03-16 10:59:25 +0100

    perf(maintainability): :recycle: Maintainability improvments on `app/js/login.js`

--- app/js/login.js
+++ app/js/login.js
@@ -46,70 +46,45 @@ function login(username,password){
         password = document.getElementById('password').value;
     }
     if(username==""||password==""){
-        document.getElementsByTagName("form")[0].classList.add("was-validated");
-        document.getElementById("ErrorMsg").innerHTML = "Error during login:<br>Please check your username and password and try again.";
-        document.getElementById("ErrorMsg").classList.remove("visually-hidden");
-        document.querySelector(`#loginSpin`).classList.add("visually-hidden")
-        document.querySelector(`#loginButton`).classList.remove("disabled")
-        return false;
+        return setError(1);
     }
     var remember = document.getElementById("remember").checked;
-    // convert password to base64
     password = btoa(password);
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status === 200) {
-            var response_obj = JSON.parse(this.responseText);
-            if(response_obj.ok === true){
-                localStorage.setItem("token", response_obj.token);
-                localStorage.setItem("user", response_obj.id);
-                window.location.href = "control-center.html";
-            }
-        }else{
-            if(this.readyState == 4){
-                const error = JSON.parse(this.responseText).error;
-                switch(this.status){
-                    case 500:
-                        if (error)
-                            document.getElementById("ErrorMsg").innerHTML = "Error during login:<br>Please check your internet connection and try again.<br>Server error: "+error;
-                        else
-                            document.getElementById("ErrorMsg").innerHTML = "Error during login:<br>Please check your internet connection and try again.";
-                        document.getElementById("ErrorMsg").classList.remove("visually-hidden");
-                        break;
-                    case 401:
-                        if (username === "guest")
-                            document.getElementById("ErrorMsg").innerHTML = "Error during login:<br>Please check your internet connection and try again.<br>You are trying to login as a guest, <br>please check you are trying to access a demo product.";
-                        else if (error)
-                            document.getElementById("ErrorMsg").innerHTML = "Error during login:<br>Please check your username and password and try again.<br>Error: "+error;
-                        else
-                            document.getElementById("ErrorMsg").innerHTML = "Error during login:<br>Please check your username and password and try again.";
-                        document.getElementById("ErrorMsg").classList.remove("visually-hidden");
-                        break;
-                    case 400:
-                        if (username === "guest") {
-                            document.getElementById("ErrorMsg").innerHTML = "Error during login:<br>Please check your internet connection and try again.<br>You are trying to login as a guest, <br>please check you are trying to access a demo product.";
-                        }else if (error)
-                            document.getElementById("ErrorMsg").innerHTML = "Error during login:<br>Please check your username and password and try again.<br>Error: "+error;
-                        else
-                            document.getElementById("ErrorMsg").innerHTML = "Error during login:<br>Please check your username and password and try again.";
-                        document.getElementById("ErrorMsg").classList.remove("visually-hidden");
-                        break;
-                    default:
-                        alert("Errore: "+this.status);
-                        break;
-                }
-                document.querySelector(`#loginSpin`).classList.add("visually-hidden")
-                document.querySelector(`#loginButton`).classList.remove("disabled")
-                document.querySelector(`#guestSpin`).classList.add("visually-hidden")
-                document.querySelector(`#guestButton`).classList.remove("disabled")
-                console.log(this.responseText);
-            }
-        }
+        if (this.readyState !== 4)
+            return;
+        var response_obj = JSON.parse(this.responseText);
+        if(this.status !== 200 || !response_obj.ok)
+            return errorReporting(response_obj.error ? response_obj.error : null,this.status,username);
+        localStorage.setItem("token", response_obj.token);
+        localStorage.setItem("user", response_obj.id);
+        window.location.href = "control-center.html";
     };
     xhttp.open("POST", "/login", true);
     xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send("{\"username\":\""+username+"\",\"password\":\""+password +"\",\"remember\":\""+remember+"\"}");
+    xhttp.send(JSON.stringify({username,password,remember}));
     return false;
+}
+
+function setError(code) {
+    if(code === 1)
+        document.getElementById("ErrorMsg").innerHTML = "Error during login:<br>Please check your username and password and try again.";
+    document.getElementsByTagName("form")[0].classList.add("was-validated");
+    document.getElementById("ErrorMsg").classList.remove("visually-hidden");
+    document.querySelector(`#loginSpin`).classList.add("visually-hidden");
+    document.querySelector(`#loginButton`).classList.remove("disabled");
+    return false;
+}
+
+function errorReporting(error,status, username) {
+    if(status === 500)
+        document.getElementById("ErrorMsg").innerHTML = "Error during login:<br>Please check your internet connection and try again.<br>Server error: " + (error ? error : "Unknown");
+    else if((status === 400 || status === 401) && username === "guest")
+        document.getElementById("ErrorMsg").innerHTML = "Error during login:<br>Please check your internet connection and try again.<br>You are trying to login as a guest, <br>please check you are trying to access a demo product.";
+    else
+        document.getElementById("ErrorMsg").innerHTML = "Error during login:<br>Please check your username and password and try again.<br>Error: " + (error ? error : "Unknown");
+    return setError();
 }
 
 //lines removed from login.html
