ed6f23f0b28745f5bc269164349f3db0a21bebef
Luca Facchini <45771235+lucafano04@users.noreply.github.com>
2023-03-16 11:38:57 +0100

    perf(maintainability): :recycle: Maintainability improvments on varius files
    List of file:
    - `app\js\admin.js`
    - `app\js\settings.js`
    - `app\js\staff.js`
    Created:
    - `app\js\redVerSess.js`

--- app/js/staff.js
+++ app/js/staff.js
@@ -1,35 +1,6 @@
 const token = localStorage.getItem('token');
 const user = localStorage.getItem('user');
 
-if (token && user) {
-	const xmlt = new XMLHttpRequest();
-	xmlt.open('POST', '/checkstat', true);
-	xmlt.setRequestHeader('Content-Type', 'application/json');
-	xmlt.send(`{"id":"${user}","token":"${token}"}`);
-	xmlt.onload = function() {
-		if (xmlt.status === 200) {
-			const response = JSON.parse(xmlt.responseText);
-			if (response.ok === false) {
-				window.location.href = '/login.html';
-				localStorage.removeItem('token');
-				localStorage.removeItem('user');
-			}
-            socket.emit("getOffices");
-		} else {
-			// User is not in a valid session
-			// Redirect to login page
-			if (xmlt.status === 500) {
-				alert('Errore: ' + xmlt.status+"\n"+JSON.parse(xmlt.responseText));
-			}
-			window.location.href = '/login.html';
-			localStorage.removeItem('token');
-			localStorage.removeItem('user');
-		}
-	}
-}else{
-	//redirect to login page
-	window.location.href = '/login.html';
-}
 
 const socket = io({
 	auth: {
